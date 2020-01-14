4cf0150e7cf5e827c70f69cad5f305593996091d
CGarcia346 <garciac3@carleton.edu>
2020-01-14 17:28:31 -0600

    finished minLink and maxLink

--- hierarchicalClustering.py
+++ hierarchicalClustering.py
@@ -1,6 +1,8 @@
 import unittest
 import math
 
+"""input(s) a list of numerical data to be recognized as a vector and outputs
+    the magnitude for the given vector1"""
 def magnitude(vector):
     sum_of_squares = 0
     for i in range(len(vector)):
@@ -8,6 +10,7 @@ def magnitude(vector):
     magnitude = math.sqrt(sum_of_squares)
     return magnitude
 
+"""input(s) two vectors and returns a vector that is the sum of the two"""
 def addVectors(vector1, vector2):
     assert(len(vector1) == len(vector2) and "Length doesn't match")
     sum_vector = []
@@ -15,18 +18,20 @@ def addVectors(vector1, vector2):
         sum_vector.append(vector1[i] + vector2[i])
     return sum_vector
 
+"""input(s) a vector and a scalar. Returns a scaled vector using the inputs"""
 def scale(vector, scalar):
     scaled_vector = []
     for i in range(len(vector)):
         scaled_vector.append(vector[i] * scalar)
     return scaled_vector
 
+"""input(s) a list/vector. returns a unit vector based off the input"""
 def unitVector(vector):
     magn = magnitude(vector)
     scalar = 1/magn
     unit = scale(vector, scalar)
     return unit
-
+"""input(s) two vectors/lists. Returns the euclidean distance between the inputs"""
 def euclideanDistance(vector1, vector2):
     assert(len(vector1) == len(vector2)), "Different size vectors"
     distance = 0
@@ -35,12 +40,14 @@ def euclideanDistance(vector1, vector2):
     distance = math.sqrt(distance)
     return distance
 
+"""input(s) a cluster and returns whether or not it has any children"""
 def hasChildren(cluster):
     if cluster.children is not None:
         return True
     return False
 
-def compareChildren(children1, children2):
+"""input(s) two clusters. Return the min_dist between them"""
+def compareChildrenMin(children1, children2):
     min_dist = math.inf
     dist = None
     #basecase
@@ -51,24 +58,27 @@ def compareChildren(children1, children2):
             return dist
 
         else:
-            for child in children2:
-                dist = compareChildren(children1, child)
+            for child in children2.children:
+                dist = compareChildrenMin(children1, child)
                 if dist < min_dist:
                     min_dist = dist
 
     else:
-        for child in children1:
-            dist = compareChildren(child, children2)
-            if dist < min_dist:
-                min_dist = dist
-        # if not hasChildren(children2):
-        #     dist = compareChildren(children1.children[0], children2)
-        #     dist = compareChildren(children1.children[1], children2)
-        # else:
-        #     dist = compareChildren(children1.children[0], children2.children[1])
-
+        if not hasChildren(children2):
+            for child in children1.children:
+                dist = compareChildrenMin(child, children2)
+                if dist < min_dist:
+                    min_dist = dist
+        else:
+            for child in children1.children:
+                for other_child in children2.children:
+                    dist = compareChildrenMin(child, other_child)
+                    if dist < min_dist:
+                        min_dist = dist
     return min_dist
-#goes through a list of clusters and gets the index of the two closest ones
+
+"""goes through a list of clusters and gets the indexes of the two closest
+    clusters according to single-link/min-link"""
 def minLink(list_of_clusters):
     min_dist = math.inf
     closest_clusters = None
@@ -76,24 +86,55 @@ def minLink(list_of_clusters):
         for j in range(i+1, len(list_of_clusters)):
             cluster_i = list_of_clusters[i]
             cluster_j = list_of_clusters[j]
-            if hasChildren(cluster_i):
-                children_i = cluster_i.children
-                children_j = cluster_j.children
-                if hasChildren(cluster_j):
-                    dist = compareChildren(children_i, children_j)
-                else:
-                    dist = compareChildren(children_i, cluster_j)
-            else:
-                if hasChildren(cluster_j):
-                    children_j = cluster_j.children
-                    dist = compareChildren(cluster_i, children_j)
-                else:
-                    dist = compareChildren(cluster_i, cluster_j)
+            dist = compareChildrenMin(cluster_i, cluster_j)
             if(dist < min_dist):
                 min_dist = dist
                 closest_clusters = (i, j)
     return closest_clusters
 
+"""input(s) two clusters. Return the max_dist between them"""
+def compareChildrenMax(children1, children2):
+    max_dist = -math.inf
+    dist = None
+    #basecase
+    if not hasChildren(children1):
+        if not hasChildren(children2):
+            dist = euclideanDistance(children1.num_vector,
+                                            children2.num_vector)
+            return dist
+
+        else:
+            for child in children2.children:
+                dist = compareChildrenMax(children1, child)
+                if dist > max_dist:
+                    max_dist = dist
+
+    else:
+        if not hasChildren(children2):
+            for child in children1.children:
+                dist = compareChildrenMax(child, children2)
+                if dist > max_dist:
+                    max_dist = dist
+        else:
+            for child in children1.children:
+                for other_child in children2.children:
+                    dist = compareChildrenMax(child, other_child)
+                if dist > max_dist:
+                    max_dist = dist
+    return max_dist
+
+def maxLink(list_of_clusters):
+        max_dist = -math.inf
+        closest_clusters = None
+        for i in range(len(list_of_clusters)):
+            for j in range(i+1, len(list_of_clusters)):
+                cluster_i = list_of_clusters[i]
+                cluster_j = list_of_clusters[j]
+                dist = compareChildrenMin(cluster_i, cluster_j)
+                if(dist > max_dist):
+                    max_dist = dist
+                    closest_clusters = (i, j)
+        return closest_clusters
 class Cluster:
   def __init__(self, numerical_vector, categorical_vector = None,
                 children = None):
@@ -108,6 +149,13 @@ def printCluster(cluster):
     print("Children: ", cluster.children)
     print("Parent: ", cluster.parent)
 
+def printDendrogram(dendrogram):
+    printCluster(dendrogram)
+    if dendrogram.children is not None:
+        for child in dendrogram.children:
+            printDendrogram(child)
+
+"""input(s) two clusters that will combine to form a new parent cluster"""
 def createNewCluster(cluster1, cluster2):
 
     list_of_chldren = [cluster1, cluster2]
@@ -116,10 +164,6 @@ def createNewCluster(cluster1, cluster2):
     cluster2.parent = newCluster
     return newCluster
 
-def getCentroid(vector1, vector2):
-    centroid = scale(addVectors(vector1, vector2), 0.5)
-    return centroid
-
 def hierarchicalClustering(data, radius = None):
     if(len(data) == 0):
         return None
@@ -127,58 +171,39 @@ def hierarchicalClustering(data, radius = None):
         only = data[0]
         return only
 
-
     #using min-link
-    # cluster1tocombine = None
-    # cluster2tocombine = None
-    # minDist = math.inf
-    # for i in range(len(data)):
-    #     cluster = data[i]
-    #     for nextCluster in data[i:]:
-    #         distance = euclideanDistance(cluster.num_data, nextCluster.num_data)
-    #         if distance < minDist:
-    #             minDist = distance
-    #             cluster1tocombine = cluster
-    #             cluster2tocombine = nextCluster
-    #     cur_magn = cluster.num_data
     clustersToCombine = minLink(data)
-    newCluster = createNewCluster(data.pop(clustersToCombine[0]), data.pop(clustersToCombine[1]))
+    cluster1 = data[clustersToCombine[0]]
+    cluster2 = data[clustersToCombine[1]]
+    data.remove(cluster1)
+    data.remove(cluster2)
+    newCluster = createNewCluster(cluster1, cluster2)
     data.append(newCluster)
-    printCluster(newCluster)
     return hierarchicalClustering(data)
-
-    # list_of_centroids = []
-    # smallest_distance, closest_clusters = minDistance(data)
-    # cluster1 = data[closest_clusters[0]]
-    # cluster2 = data[closest_clusters[1]]
-    # cluster1_num_vec = cluster1.num_vector
-    # cluster2_num_vec = cluster2.num_vector
-    # cluster3_num_vec = getCentroid(cluster1_num_vec, cluster2_num_vec)
-    # print("############################################")
-    # print("Cluster1 is: ", cluster1_num_vec)
-    # print("Cluster2 is: ", cluster2_num_vec)
-    # print("The new cluster is: ", cluster3_num_vec)
-    # print("############################################")
 
 def main():
     #open some CSV file, read through it, and create a matrix of vectors
     #assume numerical data only for now
     is_first = 1
     list_of_clusters = []
+    keys = []
     with open ('../datasets/NumericFeaturesDataSmall.csv', mode='r') as csvfile:
         for line in csvfile:
             if is_first:
                 is_first = 0
+                keys = line.split(",")
+                print(keys)
                 continue
             num_data = line.split(",")
             for i in range (len(num_data)):
-                num_data[i] = float(num_data[i])
-            list_of_clusters.append(Cluster(unitVector(num_data)))
-    hierarchicalClustering(list_of_clusters[:5])
-    # print("No items in data", hierarchicalClustering([]))
-    # print("One item in data", hierarchicalClustering([x]))
-    # print("||||||||||||||||||||||||||||||||||||||||||||")
-    # print("Multiple items, data", hierarchicalClustering(list_of_clusters))
+                if i != keys.index("person_id") and i != keys.index("decile_score"):
+                    num_data[i] = float(num_data[i])
+            num_data.pop(keys.index("decile_score"))
+            num_data.pop(keys.index("person_id"))
+            list_of_clusters.append(Cluster(num_data))
+    dendrogram = hierarchicalClustering(list_of_clusters)
+    printDendrogram(dendrogram)
+
 
 if __name__ == '__main__':
     main()
