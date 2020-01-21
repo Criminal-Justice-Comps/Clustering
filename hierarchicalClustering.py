caf9a1d1669d365f1290a6a56360dcb893f20ca3
CGarcia346 <garciac3@carleton.edu>
2020-01-21 17:14:02 -0600

    started Gower's distance

--- hierarchicalClustering.py
+++ hierarchicalClustering.py
@@ -1,6 +1,9 @@
 import unittest
 import math
 
+MINIMUM = []
+MAXIMUM = []
+RANGE = []
 """input(s) a list of numerical data to be recognized as a vector and outputs
     the magnitude for the given vector1"""
 def magnitude(vector):
@@ -31,13 +34,34 @@ def unitVector(vector):
     scalar = 1/magn
     unit = scale(vector, scalar)
     return unit
-"""input(s) two vectors/lists. Returns the euclidean distance between the inputs"""
+
+"""input(s) two numerical vectors/lists. Returns the euclidean distance between
+    the inputs"""
 def euclideanDistance(vector1, vector2):
     assert(len(vector1) == len(vector2)), "Different size vectors"
     distance = 0
     for i in range(len(vector1)):
          distance += pow(abs(vector1[i]) - abs(vector2[i]), 2)
     distance = math.sqrt(distance)
+    return distance
+
+"""input(s) two categorical vectors/lists. Returns the hamming distance"""
+def hammingDistance(vector1, vector2):
+    assert(len(vector1) == len(vector2)), "Different size vectors"
+    distance = 0
+    for i in range(len(vector1)):
+        if vector1[i] != vector2[i]:
+            distance += 1
+    return distance
+
+"""input(s) two clusters with a numerical and categorical vector. Returns the
+    Gower Distance between the two."""
+def gowerDistance(cluster1, cluster2):
+    euclid_distance = euclideanDistance(cluster1.num_vector,
+                                    cluster2.num_vector)
+    categorical_distance = hammingDistance(cluster1.cat_vector,
+                                    cluster2.cat_vector)/len(cluster1.cat_vector)
+    distance = euclid_distance + categorical_distance
     return distance
 
 """input(s) a cluster and returns whether or not it has any children"""
@@ -53,8 +77,7 @@ def compareChildrenMin(children1, children2):
     #basecase
     if not hasChildren(children1):
         if not hasChildren(children2):
-            dist = euclideanDistance(children1.num_vector,
-                                            children2.num_vector)
+            dist = gowerDistance(children1, children2)
             return dist
 
         else:
@@ -189,20 +212,53 @@ def main():
     is_first = 1
     list_of_clusters = []
     keys = []
-    with open ('../datasets/NumericFeaturesDataSmall.csv', mode='r') as csvfile:
+    with open ('../datasets/NumericFeaturesData.csv', mode='r') as csvfile:
         for line in csvfile:
+            #If it's the first line, get the column headers into "keys"
             if is_first:
                 is_first = 0
                 keys = line.split(",")
                 print(keys)
                 continue
-            num_data = line.split(",")
-            for i in range (len(num_data)):
-                if i != keys.index("person_id") and i != keys.index("decile_score"):
-                    num_data[i] = float(num_data[i])
-            num_data.pop(keys.index("decile_score"))
-            num_data.pop(keys.index("person_id"))
-            list_of_clusters.append(Cluster(num_data))
+            num_data = line.split(",") #split the line we are on into individual features
+            num_data.pop(keys.index("decile_score")) # remove the features we don't want
+            num_data.pop(keys.index("person_id")) # remove the features we don't want
+            #Set global variables MINIMUM and MAXIMUM-- these are used for Gowers Distance
+            if MINIMUM == []:
+                for i in range(len(num_data)):
+                    MINIMUM.append(math.inf)
+            if MAXIMUM == []:
+                for i in range(len(num_data)):
+                    MAXIMUM.append(-math.inf)
+            for i in range (len(num_data)): #for each feature in a row
+                num_data[i] = float(num_data[i]) #set the string to a float value (assumes numeric)
+                if num_data[i] > MAXIMUM[i]: #update max and min for each feature global (for gowers distance)
+                    MAXIMUM[i] = num_data[i]
+                if num_data[i] < MINIMUM[i]:
+                    MINIMUM[i] = num_data[i]
+            list_of_clusters.append(Cluster(num_data)) #create the cluster and add it to our list of clusters
+    for q in range(len(MAXIMUM)): #calculation to get the range of each numeric feature (used for gowers distance)
+        RANGE.append(MAXIMUM[q] - MINIMUM[q])
+    csvfile.close()
+
+    keys_categorical = []
+    cat_data = []
+    i = 0
+    print(len(list_of_clusters))
+    with open ('../datasets/CategoricalFeaturesData.csv', mode='r') as csvfile:
+        for line in csvfile:
+            print(i)
+            #If it's the first line, get the column headers into "keys"
+            if is_first:
+                is_first = 0
+                keys_categorical = line.split(",")
+                #Remove columns we don't want here
+                print(keys_categorical)
+                continue
+            cat_data = line.split(",") #split the line we are on into individual features
+            list_of_clusters[i].cat_vector = cat_data
+            i += 1
+    printCluster(list_of_clusters[0])
     dendrogram = hierarchicalClustering(list_of_clusters[:10])
     printDendrogram(dendrogram)
 
